1cca33fc7479fa3f64efba47e34f1bec33c78927
Abhin34 <107907575+Abhin34@users.noreply.github.com>
2023-03-15 00:20:01 +0530

    password bcrypted

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,18 +1,28 @@
 import { db } from "../config/db.js";
+import bcrypt from "bcrypt";
+
 export const userSignup = async (req, res) => {
-    console.log(req.body,"body");
 	const { name, email, password } = req.body;
 
-    const sqlInsert = "INSERT INTO user (name, email,password) VALUES (?,?,?)"
-    db.query(sqlInsert,[name,email,password],(err,results)=>{
-        if(err){
-            console.log(err);
-            res.status(500).json({message:"Error inserting user"})
-        }else{
-            res.status(201).json({message:"success"})
-        }
-    })
+    if(!name || !email || !password){
+        res.status(400).json({message:"Add all fields"})
+    }
 
+	//hash password
+	const salt = await bcrypt.genSalt(10);
+	const newPassword = await bcrypt.hash(password, salt);
 
-	
+	//sql query
+	const sqlInsert = "INSERT INTO user (name, email,password) VALUES (?,?,?)";
+
+	//data insertion
+	db.query(sqlInsert, [name, email, newPassword], (err, data) => {
+		if (err) {
+			console.log(err);
+			res.status(500).json({ message: "Error inserting user" });
+		} else {
+			res.status(201).json({ message: "success", user_id: data.insertId });
+		}
+	});
 };
+
